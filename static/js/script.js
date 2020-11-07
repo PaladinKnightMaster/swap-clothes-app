9d6847ed3e34939279c38263bcfea2335112515c
Liga <ligabaikova@gmail.com>
2020-11-07 22:26:04 +0000

    Style item like button

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,11 @@
 // Initialize Materialize components using jQuery
 $(document).ready(function(){
-    $('.sidenav').sidenav();
-    $('.tooltipped').tooltip();
-    $('select').formSelect();
-  });
+	$('.sidenav').sidenav();
+	$('.tooltipped').tooltip();
+	$('select').formSelect();
+});
+
+// Implement like button toggle
+$('.like-button').on( 'click', function() {
+    $(this).toggleClass('far fas') 
+});
