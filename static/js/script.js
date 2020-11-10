ca83c8b61f37fa98d574240bfdae3604ba1e8cc0
Liga <ligabaikova@gmail.com>
2020-11-10 21:56:19 +0000

    Change social icons displayed for each user Remove materialize dropdown component as it was causing bugs due to id, Implement clickable reveal component for icons using js and css classes

--- static/js/script.js
+++ static/js/script.js
@@ -3,14 +3,13 @@ $(document).ready(function(){
 	$('.sidenav').sidenav();
 	$('.tooltipped').tooltip();
 	$('select').formSelect();
-	// Dropdown for user socials
-	$('.dropdown-trigger').dropdown({
-		container: '.item-footer',
-	});
 	// scrolling effect for items on My Profile page
 	$('.carousel').carousel({
-		dist: -30
+		dist: -30,
+		shift: -20,
 	});
 });
 
-  
+$('.item-creator-container').on('click', function() {
+	$(this).next().toggleClass('visible')
+})
